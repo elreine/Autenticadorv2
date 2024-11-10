8177cc04b4b8fdfee4a6d539db420307c9c57611
elreine <reinelgarciamorales228@gmail.com>
2024-11-10 16:35:12 +0000

    Actualizar URLs del backend para Render

--- src/components/UserRegister.vue
+++ src/components/UserRegister.vue
@@ -74,7 +74,7 @@ export default {
 
     register() {
       // Enviar solicitud POST al backend con el token CSRF
-      fetch("http://127.0.0.1:5000/register", {
+      fetch("https://autenticadorv2.onrender.com/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
